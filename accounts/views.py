81e9a7aef7102cc880137c5c583e5e8c41f3e4b7
wanjikumwangib@gmail.com <wanjikumwangib@gmail.com>
2023-01-30 13:32:51 +0300

    implement serach , filter sets in user =

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from rest_framework import filters
 
 
 from .serializers import  UserSerializer
@@ -16,8 +15,18 @@ class UserListView(generics.ListAPIView):
     # permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['username', 'email', 'birth_date']
+    filterset_fields = ( 
+        'username',  
+        'email',  
+        'birth_date',  
+        'first_name',  
+        ) 
+    search_fields = ('username', 'email', 'birth_date', 'first_name',)
+
+    ordering_fields = ( 
+        'first_name', 
+        'created', 
+        ) 
 
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
